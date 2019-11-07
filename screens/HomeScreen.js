fea64943d058edd02facf2f66c4fc927d9028059
bryaldy <brylamansiang@gmail.com>
2019-11-07 15:52:50 +0800

    assignment - flat list

---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const HomeScreen = () => {
+    console.log
+
+    return (
+        <View>
+        <Text>Home Screen</Text>
+        <Button> 
+            title = "Go to Component Screen"
+            onPress={()=>{} console.log("Button 1 pressed")}
+            </Button> 
+        </View>
+    )
+}

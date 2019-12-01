94a3ff90f1f9498564a66212694d0767dd4dfcc2
bryaldy <brylamansiang@gmail.com>
2019-12-01 20:08:46 +0800

    ....

--- App.js
+++ App.js
@@ -5,15 +5,23 @@ import Maleficent from './screens/Maleficent';
 import Frozen2 from './screens/Frozen2';
 import FrozenTrailer from './screens/FrozenTrailer';
 import HomeScreen from './screens/HomeScreen';
+<<<<<<< HEAD
 import TesScreen from './screens/TesScreen';
 
 
+=======
+import TestScreen from './screens/TestScreen';
+>>>>>>> Homescreen sto
 
 
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 
 const navigator = createStackNavigator({
+<<<<<<< HEAD
   Home: TesScreen,
+=======
+  Home: TestScreen,
+>>>>>>> Homescreen sto
   Movie1: Maleficent,
   Frozen2: Frozen2,
   FrozenTrailer: FrozenTrailer,
